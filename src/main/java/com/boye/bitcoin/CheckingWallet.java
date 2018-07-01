e617b39d82323c862fa2479ecfa97347c36b2125
Bo Ye <boy.e.computer.1982@outlook.com>
2018-07-02 11:51:04 +1200

    Refine CheckingWallet application

--- src/main/java/com/boye/bitcoin/CheckingWallet.java
+++ src/main/java/com/boye/bitcoin/CheckingWallet.java
@@ -2,22 +2,18 @@ package com.boye.bitcoin;
 
 import org.bitcoinj.kits.WalletAppKit;
 import org.bitcoinj.params.TestNet3Params;
-import org.bitcoinj.wallet.UnreadableWalletException;
 import org.bitcoinj.wallet.Wallet;
 
 import java.io.File;
 
-/**
- * Created by yebo on 2/01/17.
- */
 public class CheckingWallet {
-    public static void main(String[] args) throws UnreadableWalletException {
+    public static void main(String[] args) {
         TestNet3Params params = TestNet3Params.get();
-        WalletAppKit appKit = new WalletAppKit(params, new File("."), "wallet1"); //The wallet is not to be created twice.
+        WalletAppKit appKit = new WalletAppKit(params, new File("."), "wallet"); //create a new wallet or load an existing wallet.
         appKit.startAsync();
         appKit.awaitRunning();
         System.out.println("Network connected!");
-        Wallet wallet =  appKit.wallet();
+        Wallet wallet = appKit.wallet();
         System.out.println("Wallet's current receive address: " + wallet.currentReceiveAddress());
         System.out.println("Wallet contents: " + wallet);
     }
